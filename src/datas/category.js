a9516560eb37a7e52170e83d5d83091c7d8f0274
zul-adn <zbojetx@gmail.com>
2022-08-13 13:23:41 +0700

    ADD - Category datas

---
+++ src/datas/category.js
@@ -0,0 +1,32 @@
+const category = [
+    {
+        id: '1',
+        name: 'abaya-series',
+        thumbnail: 'https://www.hijabistahub.com/site_media/img/banners/Artboard%2012_20220406101819.png'
+    },
+    {
+        id: '2',
+        name: 'aeril-zafrel-baju-melayu',
+        thumbnail: 'https://www.hijabistahub.com/site_media/img/banners/AERIL%20ZAFREL_20220324125652.png'
+    },
+    {
+        id: '3',
+        name: 'bride-series',
+        thumbnail: 'https://www.hijabistahub.com/site_media/img/banners/banner%20bride_20220107113108.png'
+    },
+    {
+        id: '4',
+        name: 'lebaran-collection-2022',
+        thumbnail: 'https://www.hijabistahub.com/site_media/img/banners/HH%20Lebaran%206%20row_20220324124545.png'
+    },
+    {
+        id: '5',
+        name: 'men-collection',
+        thumbnail: 'https://www.hijabistahub.com/site_media/img/banners/MEN-BANNER1_20211222013133.jpg'
+    },
+    {
+        id: '6',
+        name: 'casual-wear',
+        thumbnail: 'https://www.hijabistahub.com/site_media/img/banners/CASUAL_20220613162231.png'
+    },
+]
